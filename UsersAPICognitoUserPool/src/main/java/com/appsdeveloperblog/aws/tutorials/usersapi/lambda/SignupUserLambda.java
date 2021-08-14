f865f5b0c32b2d14b4f8ddd71ee6b50e11b48472
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-08-14 13:28:05 -0400

    Initial commit

---
+++ UsersAPICognitoUserPool/src/main/java/com/appsdeveloperblog/aws/tutorials/usersapi/lambda/SignupUserLambda.java
@@ -0,0 +1,30 @@
+package com.appsdeveloperblog.aws.tutorials.usersapi.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class SignupUserLambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        return response
+                .withStatusCode(200)
+                .withBody("OK");
+
+    }
+
+}
---
+++ UsersAPICognitoUserPool/src/test/java/com/appsdeveloperblog/aws/tutorials/usersapi/lambda/SignupUserLambdaTest.java
@@ -0,0 +1,22 @@
+package com.appsdeveloperblog.aws.tutorials.usersapi.lambda;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SignupUserLambdaTest {
+  @Test
+  public void successfulResponse() {
+//    SignupUserLambda signupUserLambda = new SignupUserLambda();
+//    APIGatewayProxyResponseEvent result = signupUserLambda.handleRequest(null, null);
+//    assertEquals(result.getStatusCode().intValue(), 200);
+//    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+//    String content = result.getBody();
+//    assertNotNull(content);
+//    assertTrue(content.contains("\"message\""));
+//    assertTrue(content.contains("\"hello world\""));
+//    assertTrue(content.contains("\"location\""));
+  }
+}
